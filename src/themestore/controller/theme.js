065ee38d2e24a9d784e06996bdc28cfc7a4762df
Kate605690919 <229026@whut.edu.cn>
2017-06-27 21:23:54 +0800

    主题下载功能完成
    主题列表及详情这一块，目前基本完成最小系统。还未完成的功能：详情页（预览功能，统计多人评分，下载次数，按tags筛选主题），列表页（筛选功能，分页功能）

--- src/themestore/controller/theme.js
+++ src/themestore/controller/theme.js
@@ -18,12 +18,21 @@ export default class extends Base {
   }
   async detailAction(){
     //auto render template file index_index.html
-    let id = this.get('id');
+    let id = this.get('themename');
     let listModel = this.model('list');
     //let guid = listModel.newGUID();
     //let insertId = await listModel.add({theme_uid:guid,theme_name:'test',theme_imgsrc:'test',theme_marking:5.0,theme_tags:'test-a,test-b', theme_description:'test', theme_downloadtimes:2, theme_lastupdated:'2017-06-25'});
     let data = await listModel.where({theme_name: id}).find();
+    var d = new Date(data.theme_lastupdated);  
+    data.theme_lastupdated = d.getFullYear() + '-' + (d.getMonth() + 1) + '-' + d.getDate(); 
+    data.theme_tags = data.theme_tags.split(',');
+    data.theme_lastTags = data.theme_tags.pop();
     this.assign({data: data});
     return this.display();
+  }
+  downloadAction(){
+    let themename = this.get('themename');
+    let file = think.RESOURCE_PATH + '/static/theme/'+themename;
+    this.download(file);
   }
 }
--- themelist.sql
+++ themelist.sql
@@ -1,16 +1,18 @@
+USE theme;
+DROP TABLES ts_list;
 CREATE TABLE `ts_list` (
   `theme_uid` varchar(36) NOT NULL PRIMARY KEY,
   `theme_author` varchar(20) NOT NULL,
-  `theme_vesion` varchar(15) NOT NULL,
+  `theme_version` varchar(15) NOT NULL,
   `theme_filesrc` varchar(50) NOT NULL,
   `theme_name` varchar(50) NOT NULL,
   `theme_imgsrc` varchar(50) NOT NULL,
   `theme_marking` float(2,1) DEFAULT NULL,
-  `theme_tags` varchar(100) DEFAULT NULL,
-  `theme_description` varchar(600) NOT NULL,
+  `theme_tags` varchar(400) DEFAULT NULL,
+  `theme_description` varchar(1000) NOT NULL,
   `theme_downloadtimes` smallint(6) NOT NULL,
   `theme_lastupdated` date NOT NULL
 );
-INSERT ts_list VALUES('00dc0a64-8133-c76e-156a-600a5399b1c1','Kate605690919','1.0.0.0','default','default','default',5.0,'default,black','good',1,'2017-06-25');
-INSERT ts_list VALUES('66dc0a64-5687-c76e-167b-432a5889b1ab','songzuishuai','2.0.0.1','test','test','test',5.0,'test,blank','well done',2,'2017-06-26');
+INSERT ts_list VALUES('00dc0a64-8133-c76e-156a-600a5399b1c1','Kate605690919','1.0.0.0','Twenty Seventeen.zip','Twenty Seventeen','Twenty Seventeen',5.0,'Accessibility Ready, Custom Colors, Custom Header, Custom Logo, Custom Menu, Editor Style, Featured Images, Flexible Header, Footer Widgets, One Column, Post Formats, Right Sidebar, RTL Language Support, Sticky Post, Theme Options, Threaded Comments, Translation Ready, Two Columns','Twenty Seventeen brings your site to life with header video and immersive featured images. With a focus on business sites, it features multiple sections on the front page as well as widgets, navigation and social menus, a logo, and more. Personalize its asymmetrical grid with a custom color scheme and showcase your multimedia content with post formats. Our default theme for 2017 works great in many languages, for any abilities, and on any device.',1,'2017-06-25');
+INSERT ts_list VALUES('66dc0a64-5687-c76e-167b-432a5889b1ab','songzuishuai','2.0.0.1','WPGumby.zip','WPGumby','WPGumby',5.0,'Custom Menu, Full Width Template, Left Sidebar, One Column, Right Sidebar, Theme Options, Translation Ready, Two Columns','wpGumby is a uniquely designed, professional, responsive and beautiful theme. GumbyFramework fluid grid system that adapts your website to mobile devices and the desktop or any other viewing environment. Your website will look amazing in all devices. Theme features 13 Page Templates, 5 Widget Areas, 4 Template Layouts and more. Powerful wpGumby theme options panel for full control with easy Logo Upload, Social Networking and Webmaster Tools etc. wpGumby is WooCommerce Compatible, Multilingual Ready (WPML), RTL-Language Support, Retina-Ready, Search Engine Friendly. Cross-Browser compatible. Official support forum (https://shopitpress.com/community/)',2,'2017-06-26');
 
