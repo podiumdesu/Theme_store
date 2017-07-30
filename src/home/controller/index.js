267fc5cc85796e7df57bdb704990c597f9c376aa
Kate605690919 <229026@whut.edu.cn>
2017-07-30 16:41:03 +0800

    modify
    modify

--- src/home/controller/index.js
+++ src/home/controller/index.js
@@ -23,7 +23,7 @@ export default class extends Base {
 
     //读用户缓存
     let userInfo = await this.session('userInfo');
-    this._clientId = 'f1aef8c948075819915c';
+    this._clientId = 'efaf9351830c99050b36';
     this.assign({userInfo:userInfo});
     this.assign('clientId',this._clientId);
     return this.display();
