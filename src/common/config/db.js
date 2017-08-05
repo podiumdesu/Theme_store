677a836d2fab350bfed0b3c031adb5cd8ab0d9b5
Kate605690919 <229026@whut.edu.cn>
2017-08-05 22:11:54 +0800

    modify

--- src/common/config/db.js
+++ src/common/config/db.js
@@ -11,9 +11,9 @@ export default {
     mysql: {
       host: '127.0.0.1',
       port: '3306',
-      database: 'theme',
-      user: 'root',
-      password: 'Kate6656959',
+      database: 'firekylin.org',
+      user: 'limengzhao',
+      password: 'limengzhao',
       prefix: 'ts_',
       encoding: 'utf8'
     },
--- themelist2.sql
+++ themelist2.sql
@@ -1,9 +1,8 @@
 use theme;
 DROP TABLES `ts_mark`;
 create table `ts_mark`(
-  `mark_id` int PRIMARY KEY AUTO_INCREMENT,
-  `mark_mumuid` varchar(36) NOT NULL,
-  `mark_themename` varchar(255) NOT NULL,
-  `theme_marking` float(2,1) default 0,
-  `theme_markingnum` int(11) DEFAULT 0
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `mumuid` varchar(36) NOT NULL,
+  `themename` varchar(255) NOT NULL,
+  `marking` float(2,1) default 0,
 );
