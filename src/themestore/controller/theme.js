99c1ff6a8f149a59b5426ad87e57ef4f33db7a5f
Kate605690919 <229026@whut.edu.cn>
2017-08-06 10:56:45 +0800

    modify
    modify

--- src/themestore/controller/theme.js
+++ src/themestore/controller/theme.js
@@ -37,8 +37,8 @@ export default class extends Base {
       }
       this.assign({page:array});
     }
-    themeList.data = linq.from(themeList.data).forEach(x=>x.theme_name=decodeURIComponent(x.theme_name)).toArray();
-    sessionList = linq.from(themeList.data).forEach(x=>x.theme_name=decodeURIComponent(x.theme_name)).toArray();
+    themeList.data = linq.from(themeList.data).forEach(x=>x.theme_name=decodeURIComponent(x.theme_name));
+    sessionList = linq.from(themeList.data).forEach(x=>x.theme_name=decodeURIComponent(x.theme_name));
     await this.session('type',type);
     await this.session('sessionList',sessionList);
     this.assign({themelist:themeList,CurrentPageName:'Theme List',currentUrl:encodeURIComponent(currentUrl)});
